f7603271b73e6d7a86424be1fcf6d85c08f5f819
madhuribharatula <madhuribharathula@gmail.com>
2016-11-04 23:48:41 -0700

    Add files via upload

---
+++ BasicGraph/RedColor.java
@@ -0,0 +1,35 @@
+import java.awt.Color;
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class RedColor here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class RedColor extends ColorPallet
+{
+     private Color color;
+    /**
+     * Act - do whatever the RedColor wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+    
+    
+    public Color getColor(){
+        return color;
+    }
+    public RedColor(){
+        color=Color.RED;
+        GreenfootImage img = new GreenfootImage(50, 50);
+        img.setColor(color);
+        img.drawOval(0,0,50,50);
+        img.fillOval(0,0,50,50); 
+        setImage(img);
+    
+    }
+}
